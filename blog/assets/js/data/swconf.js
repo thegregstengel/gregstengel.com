e6e5ebc829ccf1fc8feb2bcdb5e7823ac660f4fc
thegregstengel <thegregstengel@users.noreply.github.com>
2026-02-20 21:53:46 +0000

    deploy: db91acb9bae08f47b83a187c15d4dae6fd181b85

--- blog/assets/js/data/swconf.js
+++ blog/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1771610132',resources: [
+    cacheName: 'chirpy-1771624425',resources: [
       '/blog/assets/css/jekyll-theme-chirpy.css',
       '/blog/',
       
